eb57b224279121aeace1df16643ee39230737b77
daming-lu <daminglu@yahoo.com>
2014-03-02 11:30:41 -0800

    fix breaks

--- main_login.php
+++ main_login.php
@@ -3,7 +3,7 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>DreamHost</title>
+    <title>Sign in | FitnessManager</title>
     <link rel="stylesheet" href="css/foundation.css" />
     <link rel="stylesheet" href="css/login.css" />
     <script src="js/vendor/modernizr.js"></script>
